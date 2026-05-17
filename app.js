faac2eef4364378fc5ebe067b96645816c987ab5
maxmizan33 <maxmizan33@gmail.com>
2026-05-18 01:06:07 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>MyApp Production-Grade Demo</h1>
+        <p>Served by EC2 instance: ${require('os').hostname()}</p>
+    `);
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
